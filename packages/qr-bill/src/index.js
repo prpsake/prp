65195120cf94a86a34ed9e2429ee876436de0ce1
gourmetseasoningsake <jerome@shtill.com>
2022-10-14 01:45:51 +0200

    fs: add publish job 2 [Test]

--- packages/qr-bill/src/index.js
+++ packages/qr-bill/src/index.js
@@ -1,3 +1,4 @@
+// some change
 export { default as Element } from "./Element.js"
 export * as Helpers from "./Helpers.js"
 export * as Parser from "./Parser.mjs"
@@ -5,3 +6,6 @@ export * as Validator from "./Validator.mjs"
 export * as Formatter from "./Formatter.mjs"
 export * as Data from "./Data.mjs"
 export * as QRCode from "./QRCode.mjs"
+
+
+const a = 1;
