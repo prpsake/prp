43c27b8b8b70a49ebe3ff991beedbcf793a1ec61
gourmetseasoningsake <jerome@shtill.com>
2023-01-23 00:30:38 +0100

    dev: refactor example code

--- examples/example-qr-bill-code-node/index.js
+++ examples/example-qr-bill-code-node/index.js
@@ -1,14 +1,13 @@
-import fs from "fs";
+import {promises as fsp} from "fs";
 import {jsonToQrBillCode} from "@prpsake/qr-bill";
 
-const json = fs.readFileSync("public/data/qr-bill-sample.json", {
-  encoding: "utf8",
-});
-
-const {data, svg} = jsonToQrBillCode(json);
-
-if (data.error.length > 0) {
-  console.log(data.error);
-} else {
-  fs.writeFileSync("public/svg/XYZ123.svg", svg, {encoding: "utf8"});
-}
+fsp
+  .readFile("public/data/qr-bill-sample.json", {encoding: "utf8"})
+  .then(jsonToQrBillCode)
+  .then(({data, svg}) =>
+    data.error.length > 0
+      ? data.error
+      : fsp.writeFile("public/svg/XYZ123.svg", svg, {encoding: "utf8"}),
+  )
+  .then((error) => console.log(error || "success"))
+  .catch(console.log);
