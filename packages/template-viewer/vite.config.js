b1a60270e9f4703bd7e1fedb69c3472c44b7838e
gourmetseasoningsake <jerome@shtill.com>
2023-03-03 00:26:32 +0100

    dev: alpha ->  number ids

--- packages/template-viewer/vite.config.js
+++ packages/template-viewer/vite.config.js
@@ -19,7 +19,7 @@ export default ({mode}) =>
         publicDir: false,
         build: {
           target: "esnext",
-          minify: true,
+          minify: false,
           lib: {
             entry: resolve(__dirname, "src/index.js"),
             fileName: "index",
